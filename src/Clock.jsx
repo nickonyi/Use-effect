ee3b88c50a3d8e26dcf614498b99ff2466f74635
Nick <nickoluoch1130@gmail.com>
2024-04-12 15:46:30 +0300

    use effect

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
 
 export default function Clock() {
-    
+   const [counter,setCounter] = useState(0);
+   useEffect(()=> {
+    setInterval(()=>{
+        setCounter(counter => counter+ 1 );
+      },1000)
+   })
+      
+   return  (
+    <p>{counter} seconds have passed!!</p>
+   )
 }
