a1ac9e621d0a2d9bad238a1f5bedc15b9564b302
nickonyi <nickoluoch1130@gmail.com>
2024-04-15 18:23:54 +0300

    Refactor code to add chat functionality and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { createConnection } from './chat.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const serverUrl = "http://localhost:1234";
 
+function ChatRoom ({roomId}){
+   useEffect(()=>{
+    const connection = createConnection(serverUrl,roomId);
+    connection.connect();
+    return ()=> {
+      connection.disconnect();
+    }
+   },[roomId]);
+
+  return <h1>Welcome to the {roomId} room!</h1>
+}
+
+export default function App() {
+  const [roomId,setRoomId] = useState('general');
+  const [show,setShow] = useState(false);
+  
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <label>
+      Choose the chat room:{''}
+      <select value={roomId} onChange={(e)=> setRoomId(e.target.value)}>
+        <option value="general">general</option>
+        <option value="travel">travel</option>
+        <option value="music">music</option>
+      </select>
+    </label>
+
+    <button onClick={()=> {setShow(!show)}}>
+       {show?'close chat':'open chat'}
+    </button>
+    {show && <hr/>}
+    {show && <ChatRoom roomId={roomId}/>}
     </>
   )
 }
-
-export default App
--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect,useState } from "react";
 
 export default function Clock() {
-   const [counter,setCounter] = useState(0);
-   useEffect(()=> {
-    setInterval(()=>{
-        setCounter(counter => counter+ 1 );
-      },1000)
-   })
-      
-   return  (
-    <p>{counter} seconds have passed!!</p>
-   )
+  const [counter,setCounter] = useState(0);
+
+  useEffect(()=>{
+    const key =setInterval(()=>{
+      setCounter(counter => counter + 1);
+    },1000)
+  
+  return ()=> {
+    clearInterval(key);
+  }
+  },[])
+
+
+  return (
+    <p>{counter} seconds have passed!!!!</p>
+  )
 }
